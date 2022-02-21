1f7de1d7d8f636af6a34217ce3afef371376aa31
Tim de Kok <de.kok.ac@gmail.com>
2022-02-21 10:11:10 +0100

    final version

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom ({
+    key: "playlistState",
+    default: null,
+})
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: '6O5667pYWKrXCep7PWSzep',
+})

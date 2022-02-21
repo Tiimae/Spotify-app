eecbb953c077332956a0ba30dead7d5f78533ae4
Tim de Kok <de.kok.ac@gmail.com>
2022-02-21 18:28:25 +0100

    minor change to the sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -79,52 +79,54 @@ function Sidebar() {
                 </div>
             </div>
 
-            <div
-                className="
-            text-gray-500 p-5 text-xs border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hide
-            sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36
+            <div>
+                <div
+                    className="
+            text-gray-500 p-5 text-xs border-r border-gray-900 h-[88vh] overflow-y-scroll scrollbar-hide
+            sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex
             ">
 
-                <div className="space-y-4">
-                    <Link href={"/"}>
+                    <div className="space-y-4">
+                        <Link href={"/"}>
+                            <button className="flex items-center space-x-2 hover:text-white">
+                                <HomeIcon className="h-5 w-5"/>
+                                <p>Home</p>
+                            </button>
+                        </Link>
                         <button className="flex items-center space-x-2 hover:text-white">
-                            <HomeIcon className="h-5 w-5"/>
-                            <p>Home</p>
+                            <SearchIcon className="h-5 w-5"/>
+                            <p>Search</p>
                         </button>
-                    </Link>
-                    <button className="flex items-center space-x-2 hover:text-white">
-                        <SearchIcon className="h-5 w-5"/>
-                        <p>Search</p>
-                    </button>
-                    <button className="flex items-center space-x-2 hover:text-white">
-                        <LibraryIcon className="h-5 w-5"/>
-                        <p>Your Library</p>
-                    </button>
-                    <hr className="border-t-[0.1px] border-gray-900"/>
+                        <button className="flex items-center space-x-2 hover:text-white">
+                            <LibraryIcon className="h-5 w-5"/>
+                            <p>Your Library</p>
+                        </button>
+                        <hr className="border-t-[0.1px] border-gray-900"/>
 
-                    <button className="flex items-center space-x-2 hover:text-white">
-                        <PlusCircleIcon className="h-5 w-5"/>
-                        <p>Create Playlist</p>
-                    </button>
-                    <button className="flex items-center space-x-2 hover:text-white">
-                        <HeartIcon className="h-5 w-5"/>
-                        <p>Liked Songs</p>
-                    </button>
-                    <button className="flex items-center space-x-2 hover:text-white">
-                        <RssIcon className="h-5 w-5"/>
-                        <p>Your episodes</p>
-                    </button>
-                    <hr className="border-t-[0.1px] border-gray-900"/>
-                    {playlists.map((playlist) =>
-                        <p className="cursor-pointer hover:text-white" onClick={() => {
-                            setPlaylistId(playlist.id);
-                        }}>
-                            <Link href="/playlist" key={playlist.id}>
-                                {playlist.name}
-                            </Link>
-                        </p>
-                    )}
+                        <button className="flex items-center space-x-2 hover:text-white">
+                            <PlusCircleIcon className="h-5 w-5"/>
+                            <p>Create Playlist</p>
+                        </button>
+                        <button className="flex items-center space-x-2 hover:text-white">
+                            <HeartIcon className="h-5 w-5"/>
+                            <p>Liked Songs</p>
+                        </button>
+                        <button className="flex items-center space-x-2 hover:text-white">
+                            <RssIcon className="h-5 w-5"/>
+                            <p>Your episodes</p>
+                        </button>
+                        <hr className="border-t-[0.1px] border-gray-900"/>
+                        {playlists.map((playlist) =>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {
+                                setPlaylistId(playlist.id);
+                            }}>
+                                <Link href="/playlist" key={playlist.id}>
+                                    {playlist.name}
+                                </Link>
+                            </p>
+                        )}
 
+                    </div>
                 </div>
             </div>
         </div>
